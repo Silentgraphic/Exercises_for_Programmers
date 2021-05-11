ae1c08323ffc14dd8b0287677ddff2e74e476d97
silentgraphic <kierannolan1995@gmail.com>
2021-05-11 10:34:22 +0100

    Added test for concat strings

--- Intput_Processing_Output/saying_hello/saying_hello_tests/test.cpp
+++ Intput_Processing_Output/saying_hello/saying_hello_tests/test.cpp
@@ -1,6 +1,11 @@
-#include "pch.h"
+#include<string>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "pch.h"
+#include "saying_hello_header.h"
+TEST(StringConcat, ConcatTwoStrings)
+{
+	ConcatString concatString;
+	std::string string1 = "Hello ";
+	std::string string2 = "World";
+	EXPECT_EQ(concatString.(string1, string2), "Hello World");
 }
