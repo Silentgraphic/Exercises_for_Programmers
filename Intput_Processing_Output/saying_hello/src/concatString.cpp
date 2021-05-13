ca948b20a965957bf1b10f8de55aff3a54b921df
silentgraphic <kierannolan1995@gmail.com>
2021-05-13 12:21:19 +0100

    Changed concatString to allow for multiple markers

--- Intput_Processing_Output/saying_hello/src/concatString.cpp
+++ Intput_Processing_Output/saying_hello/src/concatString.cpp
@@ -1,16 +1,23 @@
 #include "saying_hello_header.h"
 #include<stdexcept>
 
-std::string concatString(std::string fullString, std::string subString)
+std::string concatString(std::string fullString, const std::string subString)
 {
-	std::string marker = "*";
-	rsize_t pos = fullString.find(marker);
+	const std::string marker = "*";
+	rsize_t pos;
+
+	pos = fullString.find(marker);
 	if (pos == std::string::npos)
 	{
-		throw std::invalid_argument("No string marker found");
+		throw std::invalid_argument("No string markers found");
 	}
 	else
 	{
-		return fullString.replace(pos, fullString.find(marker), subString);
+		while (pos != std::string::npos)
+		{
+			fullString = fullString.replace(pos, 1, subString);
+			pos = fullString.find(marker);
+		};
 	}
+	return fullString;
 }
