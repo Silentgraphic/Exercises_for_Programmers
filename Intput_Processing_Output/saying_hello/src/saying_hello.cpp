a4381df0e06c1cc016f7bb92139557013d6168d7
silentgraphic <kierannolan1995@gmail.com>
2021-05-17 15:17:41 +0100

    Added main and setup inputs and outputs

--- Intput_Processing_Output/saying_hello/src/saying_hello.cpp
+++ Intput_Processing_Output/saying_hello/src/saying_hello.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-#include<string>
+#include <string>
+#include <sstream>
+#include "saying_hello_header.h"
 
 int main()
 {
+	consoleOutput(std::cout, "Enter name:");
+	std::string name = getConsoleInput(std::cin);
+	std::string outputSentance = concatString("Hello, *, nice to meet you!", name);
+	consoleOutput(std::cout, outputSentance);
 }
