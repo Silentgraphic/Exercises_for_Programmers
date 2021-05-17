aa3642390b94379913044a60c7db7038ac55799d
silentgraphic <kierannolan1995@gmail.com>
2021-05-17 12:22:41 +0100

    Changed name of function definitions

---
+++ Intput_Processing_Output/saying_hello/src/saying_hello_functions.cpp
@@ -0,0 +1,28 @@
+#include "saying_hello_header.h"
+#include<stdexcept>
+
+std::string concatString(std::string fullString, const std::string subString)
+{
+	const std::string marker = "*";
+	rsize_t pos;
+
+	pos = fullString.find(marker);
+	if (pos == std::string::npos)
+	{
+		throw std::invalid_argument("No string markers found");
+	}
+	else
+	{
+		while (pos != std::string::npos)
+		{
+			fullString = fullString.replace(pos, 1, subString);
+			pos = fullString.find(marker);
+		};
+	}
+	return fullString;
+}
+
+std::string getConsoleInput(std::istream& input, std::string message)
+{
+	return "Hello World";
+}
