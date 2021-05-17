2103f770feb2afa07f975d447cea2356ea921fa7
silentgraphic <kierannolan1995@gmail.com>
2021-05-17 12:23:18 +0100

    Added getConsoleInput

--- Intput_Processing_Output/saying_hello/src/saying_hello_header.h
+++ Intput_Processing_Output/saying_hello/src/saying_hello_header.h
@@ -1,4 +1,7 @@
 #pragma once
 #include<string>
+#include <iostream>
 
 std::string concatString(std::string fullString, std::string subString);
+
+std::string getConsoleInput(std::istream& input, std::string message);
