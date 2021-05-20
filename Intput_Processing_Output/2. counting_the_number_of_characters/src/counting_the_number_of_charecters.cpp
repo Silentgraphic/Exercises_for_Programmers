95b90f3dc94edfc83b77732bea75e3401b994c73
silentgraphic <kierannolan1995@gmail.com>
2021-05-20 15:03:29 +0100

    Added main function

--- Intput_Processing_Output/2. counting_the_number_of_characters/src/counting_the_number_of_charecters.cpp
+++ Intput_Processing_Output/2. counting_the_number_of_characters/src/counting_the_number_of_charecters.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "counting_the_number_of_characters.h"
 
 int main()
 {
+	std::string userString = getUserString(std::cin, std::cout);
+	int userCharCount = charCount(userString);
+	charCountOutput(userString, userCharCount, std::cout);
 }
