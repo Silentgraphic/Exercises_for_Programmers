9d4eb1b4ff7b44cdb5953dbb161bc5a9bfa71992
silentgraphic <kierannolan1995@gmail.com>
2021-05-20 11:34:23 +0100

    Added input test and changed getUserString signature

--- Intput_Processing_Output/2. counting_the_number_of_characters/counting_the_number_of_characters_tests/counting_the_number_of_characters_tests.cpp
+++ Intput_Processing_Output/2. counting_the_number_of_characters/counting_the_number_of_characters_tests/counting_the_number_of_characters_tests.cpp
@@ -3,16 +3,31 @@
 #include "pch.h"
 #include "counting_the_number_of_characters.h"
 
-namespace consoleInput
+namespace Input
 {
-	TEST(ConsoleInput, ReturnInput)
+	TEST(Input, ShowsPrompt)
+	{
+		std::stringstream fakeInput;
+		std::stringstream output;
+		std::string returnedString;
+
+		std::string outputString = "What is the input string: ";
+		getUserString(fakeInput, output);
+
+		returnedString = output.str();
+
+		EXPECT_EQ(outputString, returnedString);
+	}
+
+	TEST(Input, ReturnInput)
 	{
 		std::string testString = "Hello World";
 		std::stringstream fakeInput;
+		std::stringstream output;
 
 		fakeInput << "Hello World";
 
-		std::string returnedString = getConsoleInput(fakeInput);
+		std::string returnedString = getUserString(fakeInput, output);
 		EXPECT_EQ(testString, returnedString);
 	}
 }
