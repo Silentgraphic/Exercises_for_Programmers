46033a2eb4f3663aee7eed734bfa1a1020a1dd87
silentgraphic <kierannolan1995@gmail.com>
2021-05-25 11:49:09 +0100

    Added input tests

--- Intput_Processing_Output/3. printing_quotes/printing_quotes_tests/printing_quotes_tests.cpp
+++ Intput_Processing_Output/3. printing_quotes/printing_quotes_tests/printing_quotes_tests.cpp
@@ -21,3 +21,32 @@ namespace CharacterEscapeOutput
 		EXPECT_EQ(expectedOutput, actualOutput);
 	}
 }
+
+namespace Input
+{
+	TEST(Input, ShowsPrompt)
+	{
+		std::stringstream fakeInput;
+		std::stringstream output;
+		std::string returnedString;
+
+		std::string outputString = "What is the input string: ";
+		getUserString(fakeInput, output);
+
+		returnedString = output.str();
+
+		EXPECT_EQ(outputString, returnedString);
+	}
+
+	TEST(Input, ReturnInput)
+	{
+		std::string testString = "Hello World";
+		std::stringstream fakeInput;
+		std::stringstream output;
+
+		fakeInput << "Hello World";
+
+		std::string returnedString = getUserString(fakeInput, output);
+		EXPECT_EQ(testString, returnedString);
+	}
+}
