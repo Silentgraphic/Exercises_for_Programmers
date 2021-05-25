4b77e95a191bc4bae42f565f602351d87fe27b8e
silentgraphic <kierannolan1995@gmail.com>
2021-05-25 11:59:54 +0100

    Added main function

--- Intput_Processing_Output/3. printing_quotes/printing_quotes/src/printing_quotes.cpp
+++ Intput_Processing_Output/3. printing_quotes/printing_quotes/src/printing_quotes.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "printing_quotes.h"
 
 int main()
 {
+	std::string quotePrompt = "What is the quote: ";
+	std::string namePrompt = "Who said it: ";
+
+	std::string quote = getUserString(quotePrompt, std::cin, std::cout);
+	std::string name = getUserString(namePrompt, std::cin, std::cout);
+	setEscapedOutput(name, quote, std::cout);
+	std::cin;
 }
