243e230b4ecb573488cd53e76432a543451421bc
silentgraphic <kierannolan1995@gmail.com>
2021-05-25 11:44:32 +0100

    Added setEscapedOutput function definition

--- Intput_Processing_Output/3. printing_quotes/printing_quotes/src/printing_quotes_functions.cpp
+++ Intput_Processing_Output/3. printing_quotes/printing_quotes/src/printing_quotes_functions.cpp
@@ -2,4 +2,5 @@
 
 void setEscapedOutput(std::string& name, std::string& quote, std::ostream& output)
 {
+	output << name + " says " + char(34) + quote + char(34);
 }
--- Intput_Processing_Output/3. printing_quotes/printing_quotes_tests/printing_quotes_tests.cpp
+++ Intput_Processing_Output/3. printing_quotes/printing_quotes_tests/printing_quotes_tests.cpp
@@ -11,7 +11,7 @@ namespace CharacterEscapeOutput
 		std::string name = "Foo";
 		std::string quote = "Hello world";
 		std::stringstream output;
-		std::string expectedOutput = "Foo says \" Hello World \"";
+		std::string expectedOutput = "Foo says \"Hello world\"";
 		std::string actualOutput;
 
 		setEscapedOutput(name, quote, output);
