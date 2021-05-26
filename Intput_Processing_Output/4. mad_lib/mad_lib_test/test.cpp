d13fd956cdcf3c67e2b51a1b4f8555beae97ad51
silentgraphic <kierannolan1995@gmail.com>
2021-05-26 13:11:04 +0100

    Added user prompt test

--- Intput_Processing_Output/4. mad_lib/mad_lib_test/test.cpp
+++ Intput_Processing_Output/4. mad_lib/mad_lib_test/test.cpp
@@ -1,8 +1,22 @@
 #include "pch.h"
 #include "mad_lib.h"
+#include <sstream>
 
-TEST(TestCaseName, TestName)
+namespace GetUserInput
 {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	const std::string testString = "Foo";
+	TEST(GetUserInput, PromptUser)
+	{
+		GetWords s;
+		std::stringstream fakeInput;
+		std::stringstream output;
+
+		std::string expectedString = "Enter a noun: Foo \n Enter a verb: Foo \n Enter a abjective: Foo\n Enter a abverb: Foo\n";
+
+		fakeInput << testString;
+
+		s.userPrompt(fakeInput, output);
+
+		EXPECT_EQ(expectedString, output.str());
+	}
 }
