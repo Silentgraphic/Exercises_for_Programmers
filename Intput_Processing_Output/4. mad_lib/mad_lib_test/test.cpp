bdfc4c12d5b9bf0a4991271fadf9d5ad59bce834
silentgraphic <kierannolan1995@gmail.com>
2021-05-26 13:28:01 +0100

    Changed userrPrompt to be a function rather than a class method

--- Intput_Processing_Output/4. mad_lib/mad_lib_test/test.cpp
+++ Intput_Processing_Output/4. mad_lib/mad_lib_test/test.cpp
@@ -1,21 +1,27 @@
 #include "pch.h"
 #include "mad_lib.h"
 #include <sstream>
+#include <vector>
 
 namespace GetUserInput
 {
 	const std::string testString = "Foo";
 	TEST(GetUserInput, PromptUser)
 	{
-		GetWords s;
 		std::stringstream fakeInput;
 		std::stringstream output;
+		std::vector<std::string> words[4] = {
+		{"noun"},
+		{"verb"},
+		{"Abjective"},
+		{"Adverb"}
+		};
 
-		std::string expectedString = "Enter a noun: Foo \n Enter a verb: Foo \n Enter a abjective: Foo\n Enter a abverb: Foo\n";
+		std::string expectedString = "Enter a noun: Foo\n Enter a verb: Foo\n Enter a abjective: Foo\n Enter a abverb: Foo\n";
 
 		fakeInput << testString;
 
-		s.userPrompt(fakeInput, output);
+		userPrompt(words, fakeInput, output);
 
 		EXPECT_EQ(expectedString, output.str());
 	}
