67b25f6c547a767a55f9e23d184cc8b917382640
silentgraphic <kierannolan1995@gmail.com>
2021-05-27 10:15:14 +0100

    Added second test to GetUserInput

--- Intput_Processing_Output/4. mad_lib/mad_lib_test/test.cpp
+++ Intput_Processing_Output/4. mad_lib/mad_lib_test/test.cpp
@@ -10,6 +10,7 @@ namespace GetUserInput
 	{
 		std::stringstream fakeInput;
 		std::stringstream output;
+
 		std::string words[4][2] = {
 		{"noun"},
 		{"verb"},
@@ -19,10 +20,31 @@ namespace GetUserInput
 
 		std::string expectedString = "Enter a noun:Enter a verb:Enter a Abjective:Enter a Adverb:";
 
-		fakeInput << testString;
-
 		userPrompt(words, fakeInput, output);
 
 		EXPECT_EQ(expectedString, output.str());
+	}
+
+	TEST(GetUserInput, AcceptsStringInput)
+	{
+		std::stringstream fakeInput;
+		std::stringstream output;
+
+		std::string words[4][2] = {
+		{"noun"},
+		{"verb"},
+		{"Abjective"},
+		{"Adverb"}
+		};
+
+		//God this is a bad way to do this but it's the only way I can think of
+		fakeInput << "Foo" << std::endl << "Foo" << std::endl << "Foo" << std::endl << "Foo";
+
+		userPrompt(words, fakeInput, output);
+
+		for (auto& i : words)
+		{
+			EXPECT_EQ(i[1], "Foo");
+		}
 	}
 }
