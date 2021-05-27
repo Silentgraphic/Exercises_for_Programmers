cd200a364788c2d16c5a912b350349273d0d0517
silentgraphic <kierannolan1995@gmail.com>
2021-05-27 12:03:10 +0100

    Added constructStoryOutput function definition

--- Intput_Processing_Output/4. mad_lib/mad_lib/src/mad_lib.h
+++ Intput_Processing_Output/4. mad_lib/mad_lib/src/mad_lib.h
@@ -3,3 +3,5 @@
 #include <vector>
 
 void userPrompt(std::string(&words)[4][2], std::istream& input, std::ostream& output);
+
+void constructStoryOutput(std::string& story, std::string(&words)[4][2], std::ostream& output);
