c2257f35ddf1904047c7b3b4689c4f3e0aca95a8
silentgraphic <kierannolan1995@gmail.com>
2021-05-27 14:21:34 +0100

    Added main function

--- Intput_Processing_Output/4. mad_lib/mad_lib/src/mad_lib.cpp
+++ Intput_Processing_Output/4. mad_lib/mad_lib/src/mad_lib.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include "mad_lib.h"
 
 int main()
 {
+	std::string story = "Do you Verb your Abjective Noun Adverb? That's hailarious!";
+	std::string words[4][2] = {
+		{"Noun"},
+		{"Verb"},
+		{"Abjective"},
+		{"Adverb"}
+	};
+
+	userPrompt(words, std::cin, std::cout);
+
+	constructStoryOutput(story, words, std::cout);
 }
