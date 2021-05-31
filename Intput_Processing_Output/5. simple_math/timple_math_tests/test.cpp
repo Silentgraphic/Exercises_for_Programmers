ce830f912680996b6c977645c63676416519cd10
silentgraphic <kierannolan1995@gmail.com>
2021-05-31 10:59:34 +0100

    Added tests for simple math class

--- Intput_Processing_Output/5. simple_math/timple_math_tests/test.cpp
+++ Intput_Processing_Output/5. simple_math/timple_math_tests/test.cpp
@@ -1,8 +1,31 @@
 #include "pch.h"
 #include "simple_math.h"
+#include "simple_math_class.h"
 
-TEST(TestCaseName, TestName)
+namespace SimpleMathClass
 {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	int numberone = 2;
+	int numbertwo = 2;
+	SimpleMath simpleMathTestClass(numberone, numbertwo);
+
+	TEST(SimpleMathClass, Addition)
+	{
+		int returnedInt = simpleMathTestClass.addition();
+		EXPECT_EQ(returnedInt, 4);
+	}
+	TEST(SimpleMathClass, Subtraction)
+	{
+		int returnedInt = simpleMathTestClass.subtraction();
+		EXPECT_EQ(returnedInt, 0);
+	}
+	TEST(SimpleMathClass, Mutiply)
+	{
+		int returnedInt = simpleMathTestClass.mutiply();
+		EXPECT_EQ(returnedInt, 4);
+	}
+	TEST(SimpleMathClass, Division)
+	{
+		int returnedInt = simpleMathTestClass.division();
+		EXPECT_EQ(returnedInt, 1);
+	}
 }
