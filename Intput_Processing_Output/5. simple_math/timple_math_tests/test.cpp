884e819baf8199198d2710a1ea0cc7294fecea01
silentgraphic <kierannolan1995@gmail.com>
2021-05-31 10:08:30 +0100

    Added functions file

--- Intput_Processing_Output/5. simple_math/timple_math_tests/test.cpp
+++ Intput_Processing_Output/5. simple_math/timple_math_tests/test.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
+#include "simple_math.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, TestName)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
