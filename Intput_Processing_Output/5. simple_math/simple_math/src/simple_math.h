a28d8e47aab91b2616926e2878fa1948b0995713
silentgraphic <kierannolan1995@gmail.com>
2021-06-01 12:07:47 +0100

    Added userInput function

--- Intput_Processing_Output/5. simple_math/simple_math/src/simple_math.h
+++ Intput_Processing_Output/5. simple_math/simple_math/src/simple_math.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <string>
+#include <sstream>
 
-int convertStringToInt(std::string valueToConvert);
+int convertStringToInt(std::string& valueToConvert);
+
+std::string userPrompt(std::string& prompt, std::istream& input, std::ostream& output);
--- Intput_Processing_Output/5. simple_math/simple_math/src/simple_math_functions.cpp
+++ Intput_Processing_Output/5. simple_math/simple_math/src/simple_math_functions.cpp
@@ -1,6 +1,14 @@
 #include "simple_math.h"
 
-int convertStringToInt(std::string valueToConvert)
+int convertStringToInt(std::string& valueToConvert)
 {
 	return std::stoi(valueToConvert);
+}
+
+std::string userPrompt(std::string& prompt, std::istream& input, std::ostream& output)
+{
+	std::string userInput;
+	output << prompt << std::endl;
+	input >> userInput;
+	return userInput;
 }
