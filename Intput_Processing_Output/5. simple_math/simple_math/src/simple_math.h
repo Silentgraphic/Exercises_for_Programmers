b54fcf985ca5fc1928a2db0a5da6dc58c38736fb
silentgraphic <kierannolan1995@gmail.com>
2021-06-01 11:35:01 +0100

    Added convertStringToInt function

--- Intput_Processing_Output/5. simple_math/simple_math/src/simple_math.h
+++ Intput_Processing_Output/5. simple_math/simple_math/src/simple_math.h
@@ -1,1 +1,4 @@
 #pragma once
+#include <string>
+
+int convertStringToInt(std::string valueToConvert);
---
+++ Intput_Processing_Output/5. simple_math/simple_math/src/simple_math_functions.cpp
@@ -0,0 +1,6 @@
+#include "simple_math.h"
+
+int convertStringToInt(std::string valueToConvert)
+{
+	return std::stoi(valueToConvert);
+}
