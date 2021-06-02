d371e133464fbabd8cb481c8177bede041e3416f
silentgraphic <kierannolan1995@gmail.com>
2021-06-02 16:10:26 +0100

    Changed from VS Gtest adapter to Gtest static lib

--- Intput_Processing_Output/5. simple_math/simple_math_tests/test.cpp
+++ Intput_Processing_Output/5. simple_math/simple_math_tests/test.cpp
@@ -1,6 +1,12 @@
 #include <string>
 
+//I am using Gtest as a separate project and linkinging it in the pch.h
+//instead of using the build in Gtest adapter in VS as it currently has not been
+//updated and as a result does not include Gmock
+//To add test framework back in follow this StackOverflow answer
+//https://stackoverflow.com/questions/60486110/how-to-use-googlemock-in-visual-studio
 #include "pch.h"
+
 #include "simple_math.h"
 #include "simple_math_class.h"
 
