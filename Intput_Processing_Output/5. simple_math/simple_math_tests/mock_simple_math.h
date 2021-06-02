11ff8e1ad8726c4a0a6ec960d378e4a1f6930670
silentgraphic <kierannolan1995@gmail.com>
2021-06-02 16:18:59 +0100

    Added mock SimpleMathClass

--- Intput_Processing_Output/5. simple_math/simple_math_tests/mock_simple_math.h
+++ Intput_Processing_Output/5. simple_math/simple_math_tests/mock_simple_math.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "pch.h"
+#include "simple_math_classes_interface.h"
+
+class MockSimpleMath :public ISimpleMath {
+public:
+	MOCK_METHOD(int, addition, ());
+	MOCK_METHOD(int, subtraction, ());
+	MOCK_METHOD(int, mutiply, ());
+	MOCK_METHOD(int, division, ());
+};
