02583adae2540577fdf2638022d47e21f90c7840
silentgraphic <kierannolan1995@gmail.com>
2021-06-29 10:56:23 +0100

    Added Party class declaration

---
+++ 2. Calculations/8. Pizza party/pizza_party/src/Party.cpp
@@ -0,0 +1,14 @@
+#include "Party.h"
+
+void Party::setAmountOfPizza(int amountOfPizza)
+{
+}
+
+void Party::setAmountOfPeople(int amountOfPeople)
+{
+}
+
+int* Party::dividePizza()
+{
+	return nullptr;
+}
---
+++ 2. Calculations/8. Pizza party/pizza_party/src/Party.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Party
+{
+public:
+	void setAmountOfPizza(int amountOfPizza);
+	void setAmountOfPeople(int amountOfPeople);
+	int* dividePizza();
+private:
+};
