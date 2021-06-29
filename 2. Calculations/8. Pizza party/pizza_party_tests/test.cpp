18dd6aa2824cce8166f1096dce55de9be6f6fd82
silentgraphic <kierannolan1995@gmail.com>
2021-06-29 10:55:52 +0100

    Added PartyClass tests

--- 2. Calculations/8. Pizza party/pizza_party_tests/test.cpp
+++ 2. Calculations/8. Pizza party/pizza_party_tests/test.cpp
@@ -1,6 +1,34 @@
 #include "pch.h"
+#include "Party.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+namespace PartyClass
+{
+	class PartyTest : public ::testing::Test {
+	protected:
+		void SetUp() override
+		{
+			party_.setAmountOfPizza(2);
+			party_.setAmountOfPeople(8);
+		}
+
+		Party party_;
+	};
+	TEST_F(PartyTest, DivideThePizzaUpExpectCorrectAmount)
+	{
+		int* pizzaSlicesTest = party_.dividePizza();
+		EXPECT_EQ(pizzaSlicesTest[0], 2);
+	}
+
+	TEST_F(PartyTest, DivideThePizzaUpExpectNoLeftOvers)
+	{
+		int* pizzaSlicesTest = party_.dividePizza();
+		EXPECT_EQ(pizzaSlicesTest[1], 0);
+	}
+
+	TEST_F(PartyTest, DivideThePizzaUpExpecEvenAmount)
+	{
+		int* pizzaSlicesTest = party_.dividePizza();
+		int parity = pizzaSlicesTest[0] % 2;
+		EXPECT_EQ(parity, 0);
+	}
 }
