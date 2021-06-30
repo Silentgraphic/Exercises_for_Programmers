ed5d4d53c96f4b10980d38d290f93b33df0a334d
silentgraphic <kierannolan1995@gmail.com>
2021-06-30 10:49:40 +0100

    Added extra tests for party class

--- 2. Calculations/8. Pizza party/pizza_party_tests/test.cpp
+++ 2. Calculations/8. Pizza party/pizza_party_tests/test.cpp
@@ -7,29 +7,47 @@ namespace PartyClass
 	protected:
 		void SetUp() override
 		{
-			party_.setAmountOfPizza(2);
-			party_.setAmountOfPeople(8);
+			party1_.setAmountOfPizza(2);
+			party1_.setAmountOfPeople(8);
+			party2_.setAmountOfPizza(2);
+			party2_.setAmountOfPeople(9);
 		}
 
-		Party party_;
+		Party party1_;
+		Party party2_;
 	};
-	TEST_F(PartyTest, DivideThePizzaUpExpectCorrectAmount)
+	TEST_F(PartyTest, DivideThePizzaUpExpect2)
 	{
-		int* pizzaSlices = party_.dividePizza();
+		int* pizzaSlices = party1_.dividePizza();
 		int amountOfPizzaSlices = pizzaSlices[0];
 		EXPECT_EQ(amountOfPizzaSlices, 2);
 	}
 
 	TEST_F(PartyTest, DivideThePizzaUpExpectNoLeftOvers)
 	{
-		int* pizzaSlices = party_.dividePizza();
-		EXPECT_EQ(pizzaSlices[1], 0);
+		int* pizzaSlices = party1_.dividePizza();
+		int leftoverPizzaSlices = pizzaSlices[1];
+		EXPECT_EQ(leftoverPizzaSlices, 0);
 	}
 
 	TEST_F(PartyTest, DivideThePizzaUpExpecEvenAmount)
 	{
-		int* pizzaSlices = party_.dividePizza();
+		int* pizzaSlices = party1_.dividePizza();
 		int parity = pizzaSlices[0] % 2;
 		EXPECT_EQ(parity, 0);
+	}
+
+	TEST_F(PartyTest, DivideThePizzaUpExpect1)
+	{
+		int* pizzaSlices = party2_.dividePizza();
+		int amountOfPizzaSlices = pizzaSlices[0];
+		EXPECT_EQ(amountOfPizzaSlices, 1);
+	}
+
+	TEST_F(PartyTest, DivideThePizzaUpExpect7LeftOvers)
+	{
+		int* pizzaSlices = party2_.dividePizza();
+		int leftoverPizzaSlices = pizzaSlices[1];
+		EXPECT_EQ(leftoverPizzaSlices, 7);
 	}
 }
