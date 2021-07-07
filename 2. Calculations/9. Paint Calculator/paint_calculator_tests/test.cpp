85338761e93997962384f8ed9816ff3659d2dc06
silentgraphic <kierannolan1995@gmail.com>
2021-07-07 11:52:37 +0100

    Added validate float tests and updated userInput tests

--- 2. Calculations/9. Paint Calculator/paint_calculator_tests/test.cpp
+++ 2. Calculations/9. Paint Calculator/paint_calculator_tests/test.cpp
@@ -5,6 +5,7 @@
 #include"PaintPerAreaCalculator.h"
 #include "Rectangle.h"
 #include "User_prompt.h"
+#include "ValidateFloat.h"
 
 namespace PaintPerAreaCalculatorClass
 {
@@ -46,25 +47,64 @@ namespace RectangleObject
 	}
 };
 
+namespace ValidateFloatClass
+{
+	ValidateFloat validateFloatTest;
+	const std::string testString = "foo";
+	TEST(ValidateFloatClass, DoesNotThrowException)
+	{
+		std::string testFloatAsString = "1.1";
+		EXPECT_NO_THROW(validateFloatTest.ValidateInput(testFloatAsString));
+	}
+	TEST(ValidateFloatClass, DoesThrowException)
+	{
+		EXPECT_ANY_THROW(validateFloatTest.ValidateInput(testString));
+	}
+	TEST(ValidateFloatClass, ThrowsInvalidWholeFeet)
+	{
+		try
+		{
+			validateFloatTest.ValidateInput(testString);
+		}
+		catch (std::runtime_error& err)
+		{
+			std::string expectedString = "Invalid please enter a whole foot: ";
+			EXPECT_EQ(expectedString, err.what());
+		}
+	}
+}
+
 namespace GetUserIntput
 {
-	const std::string testString = "Foo";
 	std::string prompt = "What is foo?\n";
+	ValidateFloat validateFloat;
+	const float testFloat = 1.1;
+	TEST(GetUserInput, ReturnsInvalid)
+	{
+		std::stringstream fakeInput;
+		std::stringstream output;
+		const std::string testString = "Foo";
 
+		UserInput<int> testUserInput(fakeInput, output, validateFloat);
+
+		fakeInput << testString << std::endl << 1.0;
+
+		testUserInput.promptUser("");
+
+		EXPECT_EQ("Invalid please enter a whole foot: ", output.str());
+	}
 	TEST(GetUserInput, ExpectAPromtForUser)
 	{
 		std::stringstream fakeInput;
 		std::stringstream output;
 
-		UserInput<std::string> testUserInput(fakeInput, output);
+		UserInput<std::string> testUserInput(fakeInput, output, validateFloat);
 
-		fakeInput << testString;
+		fakeInput << testFloat;
 
 		testUserInput.promptUser(prompt);
 
-		std::string expectedOutput = "What is foo?\n";
-
-		EXPECT_EQ(expectedOutput, output.str());
+		EXPECT_EQ(prompt, output.str());
 	}
 
 	TEST(GetUserInput, ReturnsInputString)
@@ -72,27 +112,13 @@ namespace GetUserIntput
 		std::stringstream fakeInput;
 		std::stringstream output;
 
-		UserInput<std::string> testUserInput(fakeInput, output);
+		UserInput<std::string> testUserInput(fakeInput, output, validateFloat);
 
-		fakeInput << testString;
+		fakeInput << testFloat;
 
 		std::string returnedString = testUserInput.promptUser(prompt);
+		std::string returnedFloatAsString = "1.1";
 
-		EXPECT_EQ(returnedString, testString);
-	}
-	TEST(GetUserInput, ReturnsInvalid)
-	{
-		std::stringstream fakeInput;
-		std::stringstream output;
-
-		UserInput<int> testUserInput(fakeInput, output);
-
-		float testFloat = 1.1;
-
-		fakeInput << testString << std::endl << 1;
-
-		testUserInput.promptUser("");
-
-		EXPECT_EQ("Invalid please enter a whole foot: ", output.str());
+		EXPECT_EQ(returnedString, returnedFloatAsString);
 	}
 }
