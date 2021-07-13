6160a9aebec65b30db34d29f185050eff4fb0209
silentgraphic <kierannolan1995@gmail.com>
2021-07-13 14:46:15 +0100

    Added TaxCal tests and changed values

--- 2. Calculations/10. Self-Checkout/self-checkoutTests/test.cpp
+++ 2. Calculations/10. Self-Checkout/self-checkoutTests/test.cpp
@@ -4,28 +4,62 @@
 #include "pch.h"
 #include "USD.h"
 #include "Item.h"
+#include "TaxCal.h"
 
 #include "MockClasses/MockCurrency.h"
+#include "MockClasses/MockItem.h"
 
 namespace USDTests {
-	USD usdTest((long int)2);
+	USD usdTest(2000);
 	TEST(USDTest, ExpectConstructorToset) {
-		EXPECT_EQ(2, usdTest.twoSignificantDigits);
+		EXPECT_EQ(2000, usdTest.twoSignificantDigits);
 	}
 }
 
 namespace ItemTests {
 	TEST(ItemTests, TotalCalculatedIs2) {
-		MockCurrency currency((long int)2);
+		MockCurrency currency(2000);
 		Item testItem(currency);
 		testItem.calculateTotal(1);
-		EXPECT_EQ(2, testItem.total);
+		EXPECT_EQ(2000, testItem.totalPrice);
 	}
 
 	TEST(ItemTests, TotalCalculatedIs4) {
-		MockCurrency currency((long int)2);
+		MockCurrency currency(2000);
 		Item testItem(currency);
 		testItem.calculateTotal(2);
-		EXPECT_EQ(4, testItem.total);
+		EXPECT_EQ(4000, testItem.totalPrice);
+	}
+}
+
+namespace TaxCalTests {
+	class  TaxCalTest : public ::testing::Test {
+	protected:
+		void SetUp() override {
+			MockItem item1;
+			MockItem item2;
+			MockItem item3;
+			item1.totalPrice = 1000;
+			item2.totalPrice = 1000;
+			item3.totalPrice = 1000;
+			mockItems.push_back(&item1);
+			mockItems.push_back(&item2);
+			mockItems.push_back(&item3);
+		}
+		void TearDown() override {
+			mockItems.clear();
+		}
+		std::vector<IItem*> mockItems;
+	};
+
+	TEST_F(TaxCalTest, CorrectTotalExTax) {
+		TaxCal testTaxCal(mockItems);
+		long int totalExTax = testTaxCal.calculateTotalExTax();
+		EXPECT_EQ(3000, totalExTax);
+	}
+	TEST_F(TaxCalTest, CorrectTotalIncTax) {
+		TaxCal testTaxCal(mockItems);
+		long int totalIncTax = testTaxCal.calculateTotalIncTax();
+		EXPECT_EQ(3165, totalIncTax);
 	}
 }
