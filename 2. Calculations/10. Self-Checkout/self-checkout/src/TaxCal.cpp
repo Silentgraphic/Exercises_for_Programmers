41f26e4df21a910fe4e6be9807f0111f4d04387a
silentgraphic <kierannolan1995@gmail.com>
2021-07-15 12:24:41 +0100

    Added TaxCal method definitions

--- 2. Calculations/10. Self-Checkout/self-checkout/src/TaxCal.cpp
+++ 2. Calculations/10. Self-Checkout/self-checkout/src/TaxCal.cpp
@@ -1,9 +1,24 @@
+#include <cmath>
+
 #include "TaxCal.h"
 
+TaxCal::TaxCal(std::vector<IItem*> itemList) :
+	itemList(itemList) {
+	calculateTax();
+}
+
 unsigned long int TaxCal::calculateTotalExTax() {
-	return 0;
+	unsigned long int total = 0;
+	for (auto& i : itemList) {
+		total += i->totalPrice;
+	}
+	return total;
 }
 
 unsigned long int  TaxCal::calculateTotalIncTax() {
-	return 0;
+	return tax + calculateTotalExTax();
+}
+
+void TaxCal::calculateTax() {
+	tax = std::round((calculateTotalExTax() * taxPercent) / 1000) * 10;
 }
--- 2. Calculations/10. Self-Checkout/self-checkout/src/TaxCal.h
+++ 2. Calculations/10. Self-Checkout/self-checkout/src/TaxCal.h
@@ -4,11 +4,12 @@
 
 class TaxCal {
 public:
-	TaxCal(std::vector<IItem*> itemList) :
-		itemList(itemList) {
-	};
+	TaxCal(std::vector<IItem*> itemList);
 	unsigned long int calculateTotalExTax();
 	unsigned long int calculateTotalIncTax();
+	unsigned long int tax;
 private:
 	std::vector<IItem*> itemList;
+	const float taxPercent = 5.5;
+	void calculateTax();
 };
