77b17897a2719d8aeb382cdc827d5b96207f0c0b
silentgraphic <kierannolan1995@gmail.com>
2021-07-19 12:45:25 +0100

    Added constructors to MockItem

--- 2. Calculations/10. Self-Checkout/self-checkoutTests/MockClasses/MockItem.h
+++ 2. Calculations/10. Self-Checkout/self-checkoutTests/MockClasses/MockItem.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "IItem.h"
+#include "ICurrency.h"
 
 class MockItem :public IItem {
 public:
+	MockItem() {};
+	MockItem(ICurrency& price) {};
 	MOCK_METHOD(void, calculateTotal, (int quantity));
 	MOCK_METHOD(void, setCurrency, (ICurrency& price));
 };
