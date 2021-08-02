df0005f4b7a4c6049befb45505f6187f4a9c0d09
silentgraphic <kierannolan1995@gmail.com>
2021-08-02 14:41:18 +0100

    Added string interpolator

---
+++ 2. Calculations/10. Self-Checkout/self-checkout/src/String_interpolation.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include <sstream>
+#include <map>
+
+class StringInter {
+public:
+	template <typename T>
+	static std::string interpolateString(std::string fullString, const std::map <std::string, T>& subStrings);
+private:
+};
+
+template<typename T>
+std::string StringInter::interpolateString(std::string fullString, const std::map<std::string, T>& subStrings) {
+	rsize_t counter = 0;
+	for (const auto& [marker, replacementWord] : subStrings) {
+		std::string word;
+		counter++;
+		rsize_t pos;
+
+		pos = fullString.find("{" + marker + "}");
+		if (pos == std::string::npos) {
+			if (counter == subStrings.size()) {
+				throw std::invalid_argument("No string markers found");
+			}
+			continue;
+		} else {
+			//convert elements to a string
+			std::stringstream subStringElement{};
+			subStringElement << subStrings.at(marker);
+
+			//split into the rest of the string
+			std::string subStringSplit;
+			subStringSplit = fullString.substr(pos + marker.size() + 2, fullString.size());
+
+			//find the marker in the fullstring and replace it
+			fullString = fullString.replace(pos, fullString.size(), subStringElement.str());
+			fullString += subStringSplit;
+
+			pos = fullString.find("{" + marker + "}");
+		}
+	}
+	return fullString;
+};
