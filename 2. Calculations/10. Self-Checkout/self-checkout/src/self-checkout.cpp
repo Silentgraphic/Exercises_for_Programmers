cb3e52699df191c593ea2faeeb0eb9dce7124d10
silentgraphic <kierannolan1995@gmail.com>
2021-08-09 11:54:15 +0100

    Added main function

--- 2. Calculations/10. Self-Checkout/self-checkout/src/self-checkout.cpp
+++ 2. Calculations/10. Self-Checkout/self-checkout/src/self-checkout.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include <map>
+
+#include "DataManager.h"
+#include "User_prompt.h"
+#include "TaxCal.h"
+#include "ValidateWholeNumber.h"
+#include "ValidatePrice.h"
+#include "SelfCheckoutOutput.h"
 
 int main() {
+	ValidateWholeNumber wholeNumberValidator;
+	ValidatePrice priceValidator;
+	UserInput userInput1(std::cin, std::cout, priceValidator);
+	UserInput userInput2(std::cin, std::cout, wholeNumberValidator);
+	DataManager datManager(userInput1, userInput2);
+	TaxCal taxCal(datManager);
+	std::map<std::string, long double> substrings = { {"subTotal",taxCal.calculateTotalExTax()}, {"tax",taxCal.calculateTax()}, {"total",taxCal.calculateTotalIncTax()} };
+	SelfCheckoutOutput checkout(std::cout);
+	checkout.printOutput(substrings, "SubTotal:${subTotal} \nTax:${tax} \nTotal:${total}");
 }
