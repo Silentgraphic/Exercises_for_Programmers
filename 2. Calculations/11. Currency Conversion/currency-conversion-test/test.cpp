19dcb2b55b09f295d8068d6db5c18aa0a4d018f7
silentgraphic <kierannolan1995@gmail.com>
2021-10-13 11:49:07 +0100

    Added Euro tests

--- 2. Calculations/11. Currency Conversion/currency-conversion-test/test.cpp
+++ 2. Calculations/11. Currency Conversion/currency-conversion-test/test.cpp
@@ -1,6 +1,19 @@
+#include <string>
+
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "Euro.h"
+#include "CurrencyConverter.h"
+
+namespace EuroTests {
+	TEST(EuroTests, ExpectConstructorTosetInt) {
+		long int test = 2000;
+		Euro euroTest(test);
+		EXPECT_EQ(2000, euroTest.twoSignificantDigits);
+	}
+	TEST(EuroTests, ExpectConstructorTosetDouble) {
+		long double test = 1.11;
+		Euro euroTest(test);
+		EXPECT_EQ(1110, euroTest.twoSignificantDigits);
+	}
 }
