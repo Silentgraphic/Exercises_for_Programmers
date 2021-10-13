36dc6fa494e9c498e291aaf6dfdf75cea1372a0c
silentgraphic <kierannolan1995@gmail.com>
2021-10-13 10:53:20 +0100

    Added Euro struct

---
+++ 2. Calculations/11. Currency Conversion/currency-conversion/currency-conversion/src/Euro.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<string>
+#include <cmath>
+
+#include "ICurrency.h"
+
+struct Euro : public ICurrency {
+	//If it is a decimal point then convert to whole number
+	Euro(const long double& priceAsFloat) : ICurrency(0) {
+		twoSignificantDigits = std::round(priceAsFloat * 1000);
+	};
+	//else just store it
+	Euro(const long int& priceInCents) :ICurrency(priceInCents) {};
+
+	//Conversion rates
+	const long double dollarsAsPercentage = 0;
+
+	std::string returnPresentableFormat();
+};
---
+++ 2. Calculations/11. Currency Conversion/currency-conversion/currency-conversion/src/euro.cpp
@@ -0,0 +1,5 @@
+#include "Euro.h"
+
+std::string Euro::returnPresentableFormat() {
+	return std::string();
+}
