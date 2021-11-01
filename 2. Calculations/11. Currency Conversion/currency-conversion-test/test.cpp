be318262d4baccf73eee645547e7978ede524024
silentgraphic <kierannolan1995@gmail.com>
2021-11-01 10:53:38 +0000

    Added basic currency converter class tests

--- 2. Calculations/11. Currency Conversion/currency-conversion-test/test.cpp
+++ 2. Calculations/11. Currency Conversion/currency-conversion-test/test.cpp
@@ -2,8 +2,11 @@
 
 #include "pch.h"
 
+#include "MockClasses/mockEuro.h"
+
 #include "Euro.h"
 #include "CurrencyConverter.h"
+#include "ICurrency.h"
 
 namespace EuroTests {
 	TEST(EuroTests, ExpectConstructorTosetInt) {
@@ -15,5 +18,33 @@ namespace EuroTests {
 		long double test = 1.11;
 		Euro euroTest(test);
 		EXPECT_EQ(1110, euroTest.twoSignificantDigits);
+	}
+}
+
+namespace CurrencyConverterTests {
+	class currencyConverterTestEuros : public ::testing::Test {
+		protected:
+			void SetUp() override {
+				euroTest_1.twoSignificantDigits = 1000;
+				euroTest_2.twoSignificantDigits = 1500;
+			}
+			MockEuro euroTest_1;
+			CurrencyConverter testCurrencyConverter_1 = CurrencyConverter(euroTest_1);
+			MockEuro euroTest_2;
+			CurrencyConverter testCurrencyConverter_2 = CurrencyConverter(euroTest_2);
+	};
+	TEST_F(currencyConverterTestEuros,WholeEurosConvertToEurosCorrectly) {
+		euroTest_1.toEuros = 1;
+		EXPECT_EQ(1000, testCurrencyConverter_1.convertToEuros());
+	}
+
+	TEST_F(currencyConverterTestEuros, FractionsOfEurosConvertToDollarsCorrectly_1) {
+		euroTest_2.toDollars = 2;
+		EXPECT_EQ(3000, testCurrencyConverter_2.convertToDollars());
+	}
+
+	TEST_F(currencyConverterTestEuros, FractionsOfConvertToDollarsCorrectly_2) {
+		euroTest_2.toDollars = 3;
+		EXPECT_EQ(4500, testCurrencyConverter_2.convertToDollars());
 	}
 }
