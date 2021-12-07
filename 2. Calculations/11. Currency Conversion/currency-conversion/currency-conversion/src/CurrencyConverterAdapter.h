9f769579613c1ada33667712d36dd87335388aed
silentgraphic <kierannolan1995@gmail.com>
2021-12-07 12:18:20 +0000

    Added userinput to adapter class

--- 2. Calculations/11. Currency Conversion/currency-conversion/currency-conversion/src/CurrencyConverterAdapter.h
+++ 2. Calculations/11. Currency Conversion/currency-conversion/currency-conversion/src/CurrencyConverterAdapter.h
@@ -3,12 +3,15 @@
 #include <memory>
 
 #include "ICurrency.h"
+#include "IUserInput.h"
 
 
 class CurrencyConverterAdapter
 {
 public:
+	CurrencyConverterAdapter(IUserInput& userInput):
+		userInput(userInput){};
 	std::unique_ptr<ICurrency> setValues();
 private:
-
+	IUserInput& userInput;
 };
